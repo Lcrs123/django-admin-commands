7ff35ec34ed289975a2495d4380aabaf2f0dcd33
Lucas Camillo Ribeiro da Silveira <lucas.silveira@bcb.gov.br>
2025-07-11 10:24:18 -0300

    Add consts

---
+++ admin_commands/consts.py
@@ -0,0 +1,4 @@
+ADMIN_COMMANDS_SETTINGS_NAME = "ADMIN_COMMANDS"
+"""Expected setting name for enabling/disabling commands in the admin panel"""
+ADMIN_COMMANDS_SETTINGS_HINT = "The setting should be a dict with keys as strings with the app names and the mapped values should be either the string literal '__all__' to show all commands for the app or an iterable of strings with the command names to show."
+"""Hint for expected setting values"""
