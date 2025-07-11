c8e349483afae368c247cfaab86700b663a8e7d1
Lucas Camillo Ribeiro da Silveira <lucas.silveira@bcb.gov.br>
2025-07-11 12:20:57 -0300

    Remove verbose_name_plural DummyCommandModel

--- src/django_admin_commands/models.py
+++ src/django_admin_commands/models.py
@@ -6,5 +6,4 @@ from django.db import models
 class DummyCommandModel(models.Model):
     class Meta:
         verbose_name = "Run Management Command"
-        verbose_name_plural = "Run Management Command"
         managed = False
