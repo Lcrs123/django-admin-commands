37461fc4e1e763669b8713ab6cd92d28d8a0cba5
Lucas Camillo Ribeiro da Silveira <lucas.silveira@bcb.gov.br>
2025-07-11 16:02:59 -0300

    Add usage form field and functionality to template

--- src/django_admin_commands/forms.py
+++ src/django_admin_commands/forms.py
@@ -1,9 +1,12 @@
 from copy import deepcopy
 
 from django import forms
-from django.core.management import get_commands
+from django.core.management import get_commands, load_command_class
 
 from .utils import AppName, CommandName, get_admin_commands, AdminCommandsSetting
+from typing import TypeAlias
+
+CommandUsage: TypeAlias = str
 
 
 def get_valid_command_choices(
@@ -20,43 +23,53 @@ def get_valid_command_choices(
         list[tuple[AppName, CommandName]]: A list of tuples with two strings, the app and respective command name,
     """
     all_commands_to_apps = get_commands()
-    choice_mapping:dict[AppName, list[str]] =  {app: [] for app, _ in admin_commands if app in all_commands_to_apps.values()}
-    for command, app in all_commands_to_apps.items():
-        if app in admin_commands and (
-            admin_commands[app] == "__all__" or command in admin_commands[app]
-        ):
-            choice_mapping[app].append(command)
     choices: list[tuple[AppName, CommandName]] = []
     for command, app in all_commands_to_apps.items():
         if app in admin_commands and (
             admin_commands[app] == "__all__" or command in admin_commands[app]
         ):
             choices.append((app, command))
-    return choices
+    return sorted(choices, key=lambda x: str(x[0]) + str(x[1]))
 
 
 VALID_COMMAND_CHOICES = get_valid_command_choices()
-OPT_GROUPS:dict[AppName, list[str]] = {
+OPT_GROUPS: dict[AppName, list[tuple[CommandName, CommandUsage]]] = {
     app: [] for app, _ in VALID_COMMAND_CHOICES
 }  # Using defaultdict does not work when iterating from template for some reason
 """Mapping from App to list of enabled commands. Used in template to group select options with optgroup html tag"""
 for app, command in VALID_COMMAND_CHOICES:
-    OPT_GROUPS[app].append(command)
-
-
-class GroupedChoiceField(forms.ChoiceField):
-    optgroups: dict[AppName, list[CommandName]]
+    command_class = load_command_class(app, command)
+    parser = command_class.create_parser(app, command)
+    OPT_GROUPS[app].append((command, parser.format_help()))
 
 
 class CommandForm(forms.Form):
     """Form for the admin run command view template"""
-    command = GroupedChoiceField(
+
+    command = forms.ChoiceField(
         choices=[(command, command) for _, command in VALID_COMMAND_CHOICES],
         required=True,
     )
+    usage = forms.CharField(
+        label="Usage",
+        required=False,
+        widget=forms.widgets.Textarea(
+            attrs={
+                "disabled": True,
+                "style": "field-sizing: content;",
+                "readonly": True,
+                "placeholder": "Command usage information",
+            }
+        ),
+    )
     args = forms.CharField(label="Arguments (optional)", required=False)
 
-    def __init__(self, *args, optgroups:dict[AppName, list[str]]=OPT_GROUPS, **kwargs) -> None:
+    def __init__(
+        self,
+        *args,
+        optgroups: dict[AppName, list[tuple[CommandName, CommandUsage]]] = OPT_GROUPS,
+        **kwargs,
+    ) -> None:
         """Returns an instance of the form.
 
         Stores the value of optgroups in the form field 'command', which can be accessed in templates with 'form.command.optgroups'
@@ -65,5 +78,6 @@ class CommandForm(forms.Form):
             optgroups (dict[AppName, list[str]], optional): Mapping from App to list of enabled commands. Defaults to OPT_GROUPS.
         """
         super().__init__(*args, **kwargs)
-        # self.command.optgroups = deepcopy(optgroups)
-        self["command"].optgroups = deepcopy(optgroups) # Deepcopy just to be sure and prevent any django strangeness from accidentally messing with the original dict in successive instantiations.
+        self["command"].optgroups = deepcopy(
+            optgroups
+        )  # Deepcopy just to be sure and prevent any django strangeness from accidentally messing with the original dict in successive instantiations.
